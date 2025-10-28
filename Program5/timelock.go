88aa75e8a39cb11759299af528ef6e64e6a1a819
CDees <bcd021@latech.edu>
2025-10-27 23:56:26 -0500

    add string extraction and concatenation

--- Program5/timelock.go
+++ Program5/timelock.go
@@ -14,6 +14,7 @@ import (
 	"time"
 	"crypto/md5"
 	"io"
+	"regexp"
 )
 
 const DEBUG = true // toggles print statements and ability to input sys time
@@ -22,7 +23,8 @@ const layout = "2006 01 02 15 04 05" // using DateTime layout from time package
 
 func main() {
 	finalElapsed := timeCalculations()
-	doubleHash(finalElapsed)
+	resultHash := doubleHash(finalElapsed)
+	fmt.Print(extractChars(resultHash))
 	
 }
 
@@ -32,7 +34,8 @@ taking the users input through redirection and does the following:
 parses the provided enoch time as a time value rather than a string 
 takes the current system time 
 compares the two to get the seconds passed 
-returns second value
+
+returns second value of elapsed time 
 */
 
 
@@ -98,12 +101,12 @@ func timeCalculations() float64{ // removed time.Time
 
 /*
 function that takes in the elapsed time from epoch and then 
-computes the md5 hash twice, extracts the first two chars 
-and backwards first two ints, concatenates, and returns 
+computes the md5 hash twice
 
+returns the hash
 */
 
-func doubleHash (elapsed float64){
+func doubleHash (elapsed float64) string{
 	
 	// need to convert the ellapsed seconds into string
 	elapsedString := fmt.Sprintf("%f", elapsed)
@@ -122,6 +125,71 @@ func doubleHash (elapsed float64){
 	
 	secondHash := hashTwo.Sum(nil)
 	
-	fmt.Printf("%x", secondHash)
+	// turn result into string 
+	resultHash := fmt.Sprintf("%x", secondHash)
+	
+	if DEBUG {
+		fmt.Printf(resultHash)
+	}
+	
+	return resultHash
+	
+}
+
+
+/*
+	takes in the given md5 hash and extracts the first 
+	two characters from left to right and the first two 
+	integers from right to left 
+	
+	returns concatenation of those two chars and ints
+*/
+
+func extractChars(hash string) string{
+	
+	// gettings first two letters (L->R)
+	
+	// looking through a-f
+	letterPattern := regexp.MustCompile(`[a-f]`)
+	// finding those letters 
+	lettersFound := letterPattern.FindAllString(hash, -1)
+	
+	// putting values into normal string form 
+	firstLetters := ""
+	firstLetters += lettersFound[0] 
+	firstLetters += lettersFound[1]
+	
+	if DEBUG {
+		fmt.Println("letters found: ", lettersFound)
+	}
+	
+	// getting last two numbers (R->L)
+	
+	// looking through 0-9
+	digitPattern := regexp.MustCompile(`\d`)
+	// finding digits 
+	digitsFound := digitPattern.FindAllString(hash, -1) // THIS GRABS ALL 
+	
+	// pick last two from all digits grabbed 
+	rightDigits := []string{} 
+	rightDigits = digitsFound[len(digitsFound)-2:]
+	
+	if DEBUG {
+		fmt.Println("initial nums found: ", rightDigits)
+	}
+	
+	// need digits right to left 
+	swap := ""
+	swap = rightDigits[1] + rightDigits[0]
+	
+	if DEBUG {
+		fmt.Println("swapped numbers: ", swap)
+	}
+	
+	
+	// concatenate and return 
+	return firstLetters + swap 
+	
+	
 	
 }
